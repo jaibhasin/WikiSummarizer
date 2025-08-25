126a5ac380c59ad9de9822040252681e63240273
Jai Bhasin <bhasinjai@gmail.com>
2025-08-26 03:33:05 +0530

    updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings
 
-
 load_dotenv()
 
 chat = ChatAnthropic(model="claude-3-5-haiku-latest")
@@ -11,3 +10,31 @@ chat = ChatAnthropic(model="claude-3-5-haiku-latest")
 embd = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 
+topic = "India"
+from b import get_data
+data = get_data(topic)
+if data == "No data found":
+    print("No data found")
+    exit()
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
+
+splits = text_splitter.split_text(data)
+
+from langchain.vectorstores import FAISS
+db = FAISS.from_texts(splits, embd)
+
+query = "Give me an introduction about " + topic
+docs = db.similarity_search(query, k=5)
+
+from langchain import hub
+
+
+rag_prompt = hub.pull("rlm/rag-prompt")
+
+chain = rag_prompt | chat
+
+response = chain.run(input_documents=docs, question=query)
+
+print(response)
--- b.py
+++ b.py
@@ -10,3 +10,11 @@ page = wiki.page("best selling books")
 print(page.title)
 # print(page.text[:1000])  # first 1000 characters
 print(len(page.text))  # length of the text
+
+
+def get_data(topic : str):
+    page = wiki.page(topic)
+    if page.exists():
+        return page.text
+    else:
+        return "No data found"
