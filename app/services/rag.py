03621b3cf734a1711932992d8760520c0d8466dc
Jai Bhasin <bhasinjai@gmail.com>
2025-08-26 16:24:05 +0530

    services created

--- app/services/rag.py
+++ app/services/rag.py
@@ -1,9 +1,75 @@
+# check if db is resest for new user 
 from langchain_anthropic import ChatAnthropic
-from langchain_google_genai import ChatGoogleGenAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import os
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
+from langchain.schema import Document
+
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
 
 load_dotenv()
 
+class RAGService :
+    def __init__(self, model = 'claude-3-5-haiku-latest'):
+        self.embeddings = GoogleGenerativeAIEmbeddings(model='models/gemini-embedding-001')
+        self.vector_store = None
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=130)
+        self.llm = ChatAnthropic(model=model)
+
+    def prepare_chunks(self, sections):
+        docs = [Document(page_content=s['text'] , metadata={"title_path":s['title_path']}) for s in sections if s['text'].strip()]
+        split_docs = self.text_splitter.split_documents(docs)
+        return split_docs
+    
+
+    def add_to_vector_store(self, docs):
+        self.vector_store = Chroma.from_documents(docs, self.embeddings , persist_directory="chroma_db")
+        self.vector_store.persist()
+    
+    def generate_response(self, title ,query):
+        if not self.vector_store:
+            return ValueError("Vector store not created !!!!")
+        
+        template = """
+                You are a neutral Wikipedia summarizer. Using the provided context from Wikipedia sections, answer the question on the Article of '{title}'.
+                
+                Guidelines:
+                - Focus on key facts: introduction, history, major sections, and impacts.
+                - Keep it objective and factual—avoid opinions.
+                - Sound little fund and engaging, like a friendly encyclopedia for users attention and curiosity.
+                - Structure the summary with: 
+                - Give a nice paragraph summary.
+                - End with notable references or controversies if relevant.
+                - Limit to 400 words max.
+                - Only If context is really insufficient, note what might be missing.
+                
+                Context:
+                {context}
+                
+                Question : {question}
+                Answer:
+                """
+        prompt = PromptTemplate(
+            input_variables=["title", "context", "question"],
+            template=template,
+        )
+        retriever = self.vector_store.as_retriever(search_type="similarity", search_kwargs={"k":6})
+
+        chain = (
+        RunnableParallel({
+            "context": retriever,        # retriever gets the query
+            "question": RunnablePassthrough(),  # pass query as-is
+            "title": RunnablePassthrough(),     # pass title as-is
+            })
+            | prompt
+            | self.llm
+            | StrOutputParser()
+        )
+
+        return chain.invoke({"title": title, "question": query})
