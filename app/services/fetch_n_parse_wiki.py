caa9b1d1c9320c70e8e149636b87574e0981b7a3
Jai Bhasin <bhasinjai@gmail.com>
2025-08-26 12:44:43 +0530

    wiki routes created

--- app/services/fetch_n_parse_wiki.py
+++ app/services/fetch_n_parse_wiki.py
@@ -6,19 +6,19 @@ from urllib.parse import urlparse, unquote
 
 
 class wiki_search: 
-    def __init__(self, user_agent = "MyApp/1.0 (https://example.com)"):
-        self.wiki = wikipediaapi.Wikipedia('en', user_agent=user_agent)
-    
+    def __init__(self, user_agent = "MyApp/1.0"):
+        self.wiki = wikipediaapi.Wikipedia(language='en', user_agent=user_agent)
+        
     def get_title(self, url):
         path = urlparse(url).path
         title = unquote(path.split('/wiki/')[-1])
-        return unquote(title.replace('_', ' '))
+        return (title.replace('_', ' '))
 
     def search_page(self , query):
         if query.startswith("http"):
             title = self.get_title(query)
         else:
-            search_results1 = wikipedia.search(query)\
+            search_results1 = wikipedia.search(query)
             if not search_results1:
                 return {"title":None , "content":None}
             title = search_results1[0]
@@ -37,13 +37,27 @@ class wiki_search:
         page = self.wiki.page(data["title"])
         sections = page.sections
 
-        output = {}
+        output = []
 
-        def a1(sections):
-            for s in sections:
-                output[s.title] = s.text
-                if s.sections:
-                    a1(s.sections)
+        # def a1(sections):
+        #     out = {}
+        #     for s in sections:
+        #         out[s.title] = {
+        #             "text": s.text,
+        #             "subsections": a1(s.sections)
+        #         }
+        #     return out
+
+        def a1(sections , prefix=""):
+            for i in sections:
+                path = prefix + i.title
+                # output[path] = i.text
+                # output[text] = i.text
+                output.append({"title_path":path , "text":i.text})
+                if i.sections:
+                    a1(i.sections , path + " > ")
+
+        # return a1(sections)
         a1(sections)
         return output   
         
